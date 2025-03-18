c2c44b1a788016eb26a1f98aab34507ef0893a55
wesho1107 <wesho1107@gmail.com>
2025-03-19 00:55:33 +0800

    Implement MainMenuViewController and ProfileViewController

---
+++ TokiToki/TokiToki/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  TokiToki
+//
+//  Created by wesho on 18/3/25.
+//
+
+
+// MainMenuViewController.swift
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    @IBOutlet weak var profileButton: UIButton!
+    @IBOutlet weak var playGameButton: UIButton!
+    @IBOutlet weak var gachaButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // Navigation actions (connect to storyboard)
+    @IBAction func profileButtonTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func playGameButtonTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func gachaButtonTapped(_ sender: UIButton) {
+    }
+}
---
+++ TokiToki/TokiToki/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  TokiToki
+//
+//  Created by wesho on 18/3/25.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
